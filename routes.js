8e2e3505c5d6858f52e34f6486c00c9977804c53
HyunM <wisek0226@gmail.com>
2021-02-10 07:16:08 -0800

    first

---
+++ routes.js
@@ -0,0 +1,103 @@
+/*!
+
+=========================================================
+* * NextJS Material Dashboard v1.1.0 based on Material Dashboard React v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/nextjs-material-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-dashboard/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+import Unarchive from "@material-ui/icons/Unarchive";
+import Language from "@material-ui/icons/Language";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: Dashboard,
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: Person,
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/table-list",
+    name: "Table List",
+    rtlName: "قائمة الجدول",
+    icon: "content_paste",
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    rtlName: "طباعة",
+    icon: LibraryBooks,
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    rtlName: "الرموز",
+    icon: BubbleChart,
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    rtlName: "خرائط",
+    icon: LocationOn,
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    rtlName: "إخطارات",
+    icon: Notifications,
+
+    layout: "/dashboard",
+  },
+  {
+    path: "/rtl-page",
+    name: "RTL Support",
+    rtlName: "پشتیبانی از راست به چپ",
+    icon: Language,
+
+    layout: "/rtl",
+  },
+  {
+    path: "/upgrade-to-pro",
+    name: "Upgrade To PRO",
+    rtlName: "التطور للاحترافية",
+    icon: Unarchive,
+
+    layout: "/dashboard",
+  },
+];
+
+export default dashboardRoutes;
