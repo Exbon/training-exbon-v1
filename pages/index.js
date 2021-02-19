d18e7040d5cdf3736c0cde90cec7da6ed7648e60
HyunM <wisek0226@gmail.com>
2021-02-19 10:10:08 -0800

    directly go to login page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Router from "next/router";
 
 export default function Index() {
   React.useEffect(() => {
-    Router.push("/home/dashboard");
+    Router.push("/login");
   });
 
   return <div />;
