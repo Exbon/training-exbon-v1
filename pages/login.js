e364f7c0b0bb50b659b6bf4a23ee9e94eb66f237
HyunM <wisek0226@gmail.com>
2021-02-19 10:40:44 -0800

    Login form moved

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,20 @@
 import React from "react";
 import styles from "assets/jss/nextjs-material-dashboard/New/loginStyle.js";
 import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+// layout for this page
+import Admin from "layouts/Admin.js";
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+
+import avatar from "assets/img/faces/marc.jpg";
 
 const login = () => {
   const useStyles = makeStyles(styles);
@@ -8,7 +22,43 @@ const login = () => {
 
   return (
     <div>
-      <h1 className={classes.title}>test</h1>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={3}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>Login</h4>
+              <p className={classes.cardCategoryWhite}>Dashboard</p>
+            </CardHeader>
+            <CardBody>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CustomInput
+                    labelText="Username"
+                    id="username"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CustomInput
+                    labelText="Password"
+                    id="password"
+                    formControlProps={{
+                      fullWidth: true,
+                    }}
+                  />
+                </GridItem>
+              </GridContainer>
+            </CardBody>
+            <CardFooter>
+              <Button color="primary">Login</Button>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
     </div>
   );
 };
