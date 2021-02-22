fd0f4a2c492d7f6d63ee89a999189907c910f2a3
HyunM <wisek0226@gmail.com>
2021-02-22 08:16:34 -0800

    directly access page using cookie

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Router from "next/router";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 export default function Index() {
+  const [cookies, setCookie, removeCookie] = useCookies("username");
   React.useEffect(() => {
-    Router.push("/login");
+    {
+      cookies.username ? Router.push("/home/dashboard") : Router.push("/login");
+    }
   });
 
   return <div />;
