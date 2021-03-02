fec67e8fd2c9bc029ce74dc1115332bcd027a4f6
HyunM <wisek0226@gmail.com>
2021-03-02 13:41:33 -0800

    npm install for Task Completion

--- pages/home/dailyreport/task-completion.js
+++ pages/home/dailyreport/task-completion.js
@@ -1,6 +1,31 @@
 import Admin from "layouts/Admin.js";
+import { useState, useMemo, useEffect } from "react";
+import axios from "axios";
+import { useTable, useBlockLayout } from "react-table";
+import DateFnsUtils from "@date-io/date-fns";
+import { formatDate } from "../../../components/New/formatDate";
+import TextField from "@material-ui/core/TextField";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { deepOrange, blue } from "@material-ui/core/colors";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import { toast } from "react-toastify";
+import { usePromiseTracker, trackPromise } from "react-promise-tracker";
+import Loader from "react-loader-spinner";
+import EventBusyIcon from "@material-ui/icons/EventBusy";
+import Modal from "react-modal";
+import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
+import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
+import Router, { useRouter } from "next/router";
+import NotPermission from "./NotPermission";
+import { CookiesProvider, useCookies } from "react-cookie";
+
+import "./task-completion.css";
+
 const TaskCompletion = () => {
-  return <div></div>;
+  return <></>;
 };
+
 TaskCompletion.layout = Admin;
 export default TaskCompletion;
