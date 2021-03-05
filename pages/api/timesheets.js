e76d6560091db91806c6552491c1fba996d65e6e
HyunM <wisek0226@gmail.com>
2021-03-05 13:22:55 -0800

    timesheet api added

---
+++ pages/api/timesheets.js
@@ -0,0 +1,79 @@
+const mssql = require("mssql");
+const dbserver = require("../../dbConfig.js");
+
+const timesheetHandler = (req, res) => {
+  const { method, body } = req;
+  return new Promise(resolve => {
+    switch (method) {
+      case "GET":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const selectedDate = req.query.selectedDate;
+          const projectID = req.query.projectID;
+
+          const query = `EXEC [Hammer].[dbo].[Timesheet_SelectByDate]
+          '${selectedDate}',  ${projectID}`;
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+            res.status(200).json({
+              result: recordset.recordsets,
+            });
+            return resolve();
+          });
+        });
+        break;
+
+      case "POST":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const query = `EXEC [Hammer].[dbo].[Timesheet_Insert]
+          ${body.ProjectID}, ${body.EmployeeID}, "${body.Position}", "${body.Date}", "${body.WorkStart}", "${body.WorkEnd}", "${body.MealStart}", "${body.MealEnd}" `;
+          /* --Params--
+          @projectID int,
+          @employeeID int,
+          @position nvarchar(100),
+          @date date,
+          @workStart time(0),
+          @workEnd time(0),
+          @mealStart time(0),
+          @mealEnd time(0)
+          */
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+            res.status(200).json({
+              message: "Success, the timesheet has been created.",
+              TimesheetID: recordset.recordset[0].TimesheetID,
+            });
+            return resolve();
+          });
+        });
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        res.status(404).end(`Failed`);
+        resolve();
+    }
+  });
+};
+
+export default timesheetHandler;
---
+++ pages/api/timesheets/[TimesheetID].js
@@ -0,0 +1,104 @@
+const mssql = require("mssql");
+const dbserver = require("../../../dbConfig.js");
+
+const TimesheetIDHandler = (req, res) => {
+  const {
+    query: { TimesheetID },
+    body,
+    method,
+  } = req;
+
+  return new Promise(resolve => {
+    switch (method) {
+      // case "GET":
+      //   mssql.connect(dbserver.dbConfig, err => {
+      //     if (err) {
+      //       console.error(err);
+      //       return resolve();
+      //     }
+      //     const request = new mssql.Request();
+
+      //     const query = `SELECT [TimesheetID]
+      //                     FROM [Hammer].[dbo].[Timesheet]
+      //                     WHERE TimesheetID = ${TimesheetID}`;
+
+      //     request.query(query, (err, recordset) => {
+      //       if (err) {
+      //         console.error(err);
+      //         return resolve();
+      //       }
+
+      //       res.status(200).json(recordset.rowsAffected);
+      //       return resolve();
+      //     });
+      //   });
+      //   break;
+      case "PUT":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const query = `EXEC [Hammer].[dbo].[Timesheet_UpdateByTimesheetID]
+          ${TimesheetID}, ${body.EmployeeID}, "${body.Position}", "${body.WorkStart}",  "${body.WorkEnd}", "${body.MealStart}", "${body.MealEnd}"`;
+          /* --Params--
+          @timesheetID int,
+          @employeeID int,
+          @position nvarchar(100),
+          @workStart time(0),
+          @workEnd time(0),
+          @mealStart time(0),
+          @mealEnd time(0)
+          */
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+
+            res.status(200).json({
+              message: "Success, the timesheet has been updated.",
+            });
+            return resolve();
+          });
+        });
+        break;
+      case "DELETE":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const query = `EXEC [Hammer].[dbo].[Timesheet_DeleteByTimesheetID]
+                          ${TimesheetID}`;
+          /* --Params--
+          @timesheetID int
+          */
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+
+            res.status(200).json({
+              message: "Success, the timesheet has been deleted.",
+            });
+            return resolve();
+          });
+        });
+        break;
+      default:
+        res.setHeader("Allow", ["PUT", "DELETE"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        res.status(500).end("This is an error");
+        return resolve();
+    }
+  });
+};
+export default TimesheetIDHandler;
