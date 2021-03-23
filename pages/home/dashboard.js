3fa9cc2ed511b434b43ef61aa96803af8feee88b
HyunM <wisek0226@gmail.com>
2021-03-23 13:12:12 -0700

    user cannot be found in Dashboard

--- pages/home/dashboard.js
+++ pages/home/dashboard.js
@@ -176,31 +176,59 @@ const Dashboard = () => {
             },
           }).then(response => {
             const employeeInfo = response.data.result.recordsets[0][0];
-            setCookie("fullname", employeeInfo.FullName, {
-              path: "/",
-              maxAge: 3600 * 24 * 30,
-            });
-            setCookie("password", router.query.pw, {
-              path: "/",
-              maxAge: 3600 * 24 * 30,
-            });
-            setCookie("username", employeeInfo.UserName, {
-              path: "/",
-              maxAge: 3600 * 24 * 30,
-            });
-            setCookie("employeeid", employeeInfo.EmployeeID, {
-              path: "/",
-              maxAge: 3600 * 24 * 30,
-            });
-            setStatus(prevState => ({
-              ...prevState,
-              cookies: {
-                username: employeeInfo.UserName,
-                password: router.query.pw,
-                fullname: employeeInfo.FullName,
-                employeeid: employeeInfo.EmployeeID,
-              },
-            }));
+            if (employeeInfo !== undefined) {
+              setCookie("fullname", employeeInfo.FullName, {
+                path: "/",
+                maxAge: 3600 * 24 * 30,
+              });
+              setCookie("password", router.query.pw, {
+                path: "/",
+                maxAge: 3600 * 24 * 30,
+              });
+              setCookie("username", employeeInfo.UserName, {
+                path: "/",
+                maxAge: 3600 * 24 * 30,
+              });
+              setCookie("employeeid", employeeInfo.EmployeeID, {
+                path: "/",
+                maxAge: 3600 * 24 * 30,
+              });
+              setStatus(prevState => ({
+                ...prevState,
+                cookies: {
+                  username: employeeInfo.UserName,
+                  password: router.query.pw,
+                  fullname: employeeInfo.FullName,
+                  employeeid: employeeInfo.EmployeeID,
+                },
+              }));
+            } else {
+              setData(() => [
+                {
+                  ProjectGroup: "",
+                  ProjectID: "",
+                  ProjectName: "The user cannot be found.",
+                  ContractAmount: "",
+                  Director: "",
+                  PIC: "",
+                  Associate1: "",
+                  Associate2: "",
+                  Associate3: "",
+                  Position: "",
+                  BaselineCost: "",
+                  EEAC: "",
+                  CV: "",
+                  CPI: "",
+                  ESPI: "",
+                  WorkCompletion: "",
+                  StartDate: "",
+                  FinishDate: "",
+                  Deadline: "",
+                  IsDesign: "",
+                  DesignParentID: "",
+                },
+              ]);
+            }
           });
         } else {
           setStatus(prevState => ({
