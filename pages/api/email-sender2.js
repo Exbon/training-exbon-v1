20207d0bc53b889642059696cd0cded6011d85ac
HyunM <wisek0226@gmail.com>
2021-10-22 14:51:26 -0700

    pdf email complete

--- pages/api/email-sender2.js
+++ pages/api/email-sender2.js
@@ -11,12 +11,19 @@ const emailHandler = (req, res) => {
     },
     from: emailConfig.emailConfig.user,
     to: body.username + "@exbon.com",
-    subject: "IT TRAINING PROGRAM TEST",
-    html: "<b>This is bold text</b>",
+    subject: "Layout conflict",
+    html: `<p>Exbon,</p>
+           <p>While we are working on new wall layout, we found there is one existing light fixture conflicting with this new layout. Please inform us how to proceed. </p>
+           <p>T-Wall Enterprise</p>`,
     text: "This is text version!",
     // replyTo: "receiverXXX@gmail.com",
-    // attachments: [
-    // ],
+    attachments: [
+      {
+        filename: "Photo.pdf",
+        path: "./assets/img/training/Photo.pdf",
+        contentType: "application/pdf",
+      },
+    ],
     onError: e => console.log(e),
     onSuccess: i => console.log(i),
   });
